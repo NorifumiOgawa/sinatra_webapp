116633c128d473a786577e800a8794771334e880
NorifumiOgawa <n@ogaworks.com>
2021-07-19 14:30:33 +0900

    ファイル更新系をclassにしてみた

--- memo.rb
+++ memo.rb
@@ -14,17 +14,15 @@ end
 get '/' do
   # TOPページ
   @page_title = 'TOP'
-  @memos = []
-  memo_lists
+  memo = Memo.new
+  @memo_list = memo.list
   erb :index
 end
 
 get %r{/memo/(\d+)} do |file|
   # メモを表示するページ
-  File.open("./data/#{file}") do |f|
-    @memo = JSON.parse(f.read)
-  end
-  @filename = file
+  memo = Memo.new
+  @content = memo.read(file)
   erb :show
 end
 
@@ -36,50 +34,70 @@ end
 
 post '/memo/' do
   # 新しいメモを生成する
-  save_memo(params: params)
+  memo = Memo.new
+  memo.save(params: params)
   redirect to('/')
 end
 
 get %r{/memo/(\d+)/edit} do |file|
   # 既存のメモを編集するページ
-  File.open("./data/#{file}") do |f|
-    @memo = JSON.parse(f.read)
-  end
-  @filename = file
+  @page_title = 'Edit memo'
+  memo = Memo.new
+  @content = memo.read(file)
   erb :edit
 end
 
 patch '/memo/' do
   # 既存のメモを更新する
-  save_memo(filename: params['filename'], params: params)
+  memo = Memo.new
+  memo.save(filename: params['filename'], params: params)
   redirect to("/memo/#{params['filename']}")
 end
 
 delete '/memo/' do
   # 既存のメモを削除する
-  File.delete("./data/#{params['filename']}")
+  memo = Memo.new
+  memo.delete(params['filename'])
   redirect to('/')
 end
 
-def memo_lists
-  @file_list = Dir.glob('*', base: './data/')
-  @file_list.each do |file|
+not_found do
+  @page_title = '404 not found'
+  erb :'404'
+end
+
+class Memo
+  def list
+    memos = []
+    file_list = Dir.glob('*', base: './data/')
+    file_list.each do |file|
+      File.open("./data/#{file}") do |f|
+        hash = JSON.parse(f.read)
+        hash['title'] = 'NO TITLE' if hash['title'] == ''
+        hash['file_name'] = file
+        memos << hash
+      end
+    end
+    memos
+  end
+
+  def read(file)
+    memo = ''
     File.open("./data/#{file}") do |f|
-      hash = JSON.parse(f.read)
-      hash['title'] = 'NO TITLE' if hash['title'] == ''
-      @memos << hash
+      memo = JSON.parse(f.read)
+      memo['file_name'] = file
+    end
+    memo
+  end
+
+  def save(filename: Time.now.to_i.to_s, params: [])
+    File.open("./data/#{filename}", 'w') do |file|
+      json = { title: params['title'], body: params['body'] }
+      JSON.dump(json, file)
     end
   end
-end
 
-def save_memo(filename: Time.now.to_i.to_s, params: [])
-  File.open("./data/#{filename}", 'w') do |file|
-    json = { title: params['title'], body: params['body'] }
-    JSON.dump(json, file)
+  def delete(file)
+    File.delete("./data/#{file}")
   end
-end
-
-not_found do
-  @page_title = '404 not Found'
-  erb :'404'
 end
